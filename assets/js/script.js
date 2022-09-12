f4dfa1be8cce8961ab663fcc8bbf603e1f123ba5
Baptistemarie25 <mariebaptiste@Maries-MacBook-Air.local>
2022-09-11 20:38:37 -0400

    edited script

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,100 @@ const convertToLatLong = async (city) => {
         console.log(error);
     }
 };
+
+const fetchCurrentWeatherData = async (array) => {
+    try {
+        const apiKey = "a53b263fd5fb250576a1fa55d6798ef5";
+        const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${array[0]}&lon=${array[1]}&appid=${apiKey}`;
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+  
+const displayCurrentWeatherData = (city, data) => {
+    const cityNameEl = document.getElementById("city-name");
+    const humidityEl = document.getElementById("humidity-value");
+    const temperatureEl = document.getElementById("temperature-value");
+    const uvIndexEl = document.getElementById("uvIndex-value");
+    const windSpeedEl = document.getElementById("windSpeed-value");
+    const iconEl = document.getElementById("current-weather-icon");
+    iconEl.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`
+        );
+        cityNameEl.textContent = city.replace("-", " ");
+        humidityEl.textContent = data.current.humidity;
+        temperatureEl.textContent = convertTemptoFarenheit(data.current.temp);
+        if (parseFloat(data.current.uvi) < 4) {
+        uvIndexEl.style.backgroundColor = "green";
+        } else if (
+        parseFloat(data.current.uvi) >= 4 &&
+        parseFloat(data.current.uvi) < 8
+        ) {
+        uvIndexEl.style.backgroundColor = "yellow";
+        } else {
+        uvIndexEl.style.backgroundColor = "red";
+        }
+        uvIndexEl.textContent = data.current.uvi;
+        windSpeedEl.textContent = data.current.wind_speed;
+};
+
+const addSearchHistory = function (city) {
+    var historyButtonEl = document.createElement("button");
+    historyButtonEl.classList = "history-button btn-primary my-1 w-100 rounded";
+    historyButtonEl.textContent = city.replace("-", " ");
+    historyButtonEl.addEventListener("click", () => {
+        var city = historyButtonEl.textContent;
+        var formattedCity = city.replace(" ", "-");
+        searchCity(formattedCity);
+    });
+    searchHistoryEl.prepend(historyButtonEl);
+    searchHistoryArray.push(city.replace("-", " "));
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArray));
+};
+
+const searchCity = async (formattedCity) => {
+    const latLong = await convertToLatLong(formattedCity);
+    const currentWeatherData = await fetchCurrentWeatherData(latLong);
+    displayCurrentWeatherData(formattedCity, currentWeatherData);
+    displayFutureWeatherData(currentWeatherData);
+    addSearchHistory(formattedCity);
+    weatherContainerEl.style.display = "block";
+};
+
+const reloadButtons = () => {
+    var historyArray = JSON.parse(localStorage.getItem("searchHistory"));
+    if (historyArray != null || historyArray != undefined) {
+        for (i = historyArray.length - 1; i >= 0; i--) {
+            var historyButtonEl = document.createElement("button");
+            historyButtonEl.classList =
+            "history-button btn-primary my-1 w-100 rounded";
+            historyButtonEl.textContent = historyArray[i];
+            historyButtonEl.addEventListener("click", (e) => {
+            var buttonClicked = e.target;
+            var city = buttonClicked.textContent;
+            var formattedCity = city.replace(" ", "-");
+            searchCity(formattedCity);
+            });
+            searchHistoryEl.appendChild(historyButtonEl);
+        }
+    }
+};
+
+const convertTemptoFarenheit = (kelvin) => {
+    var tempCelsius = kelvin - 273;
+    var tempFarenheit = Math.floor((9 / 5) * tempCelsius + 32);
+    return tempFarenheit;
+};
+
+searchButtonEl.addEventListener("click", () => {
+    var city = searchInputEl.value;
+    var formattedCity = city.replace(" ", "-");
+    searchCity(formattedCity);
+});
+    
+reloadButtons();
